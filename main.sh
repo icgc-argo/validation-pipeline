a6aa665b86d51e260c82a4143067083ad48a92e6
Junjun Zhang <junjun.ca@gmail.com>
2020-10-28 12:18:47 -0400

    adjustment and added params and placehold scripts

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 
 ## INPUTS
 # 1. reference genome: GRCh38
-# 2. BED file with targeted regions (lifted from GRCh37)
-# 3. aligned targeted deep sequencing BAMs for tumour and matched normal
+# 2. donor level BED file with targeted regions (lifted from GRCh37)
+# 3. aligned targeted deep sequencing BAMs for tumour and matched normal samples
 # 4. VCF with SNV calls to be validated
 # 5. VCF with InDel calls to be validated
 
@@ -13,10 +13,32 @@
 
 
 ## Major steps
-# 1. get BAM stats, BAM depth (run-bam-stats, input: lifted BED for captured regions, aligned target seq bams)
-# 2. build BED files (input: variant call VCFs, lifted BED for captured regions)
-# 3. add read counts for SNVs (run-readcounts, input: bed, bams)
-# 4. add SGA annotation for InDels (run-sga-annotate, input: vcf (selected region only), bams)
-# 5. add read counts for SVs (run-sv-count), not now
-# 6. make calls (run-call, input: count annotated vcf), ie, categorize each call into LOWDEPTH, NOTSEEN, STRANDBIAS, NORMALEVIDENCE, GERMLINE, PASS
+# 1. get BAM depth stats (original script bam-depth.sh, input: lifted BED for captured regions, aligned target seq bams)
+# 2. add read counts for SNVs (original script one-readcount.sh, input: SNV vcf, bams)
+# 3. categorize each SNV call into LOWDEPTH, NOTSEEN, STRANDBIAS, NORMALEVIDENCE, GERMLINE,
+#    PASS (original script snv_readcounts.py, input: selected SNV VCF, normal readcount, tumour read count)
+# 4. add SGA annotation for InDels (original script one-sga-annotate.sh, input: InDel vcf (selected region only), bams)
+# 5. categorize each InDel call into LOWDEPTH, NOTSEEN, STRANDBIAS, NORMALEVIDENCE, GERMLINE,
+#    PASS (original script snv_indel_call.py, input: annotated InDel VCF produced by the previous step)
+# 6. (later) add read counts for SVs (original script count-sv-support.py, input: SV vcf (selected region only), bams)
+# 7. (later) categorize each SV call into LOWDEPTH, NOTSEEN, STRANDBIAS, NORMALEVIDENCE, GERMLINE,
+#    PASS (original script sv_call.py, input: annotated SV VCF produced by the previous step)
 
+
+# input params
+if [ ! $# -eq 8 ]
+then
+    echo "$0 - performs validation on SNV/INDEL calls by verify against deep targeted sequencing data"
+    echo "Usage: $0 REF_GENOME_FA TUMOUR_DEEP_SEQ_BAM NORMAL_DEEP_SEQ_BAM SNV_VCF INDEL_VCF DONOR_ID WORK_DIR OUTPUT_DIR"
+    echo
+    exit
+fi
+
+REF_GENOME_FA=$1
+TUMOUR_DEEP_SEQ_BAM=$2
+NORMAL_DEEP_SEQ_BAM=$3
+SNV_VCF=$4
+INDEL_VCF=$5
+DONOR_ID=$6
+WORK_DIR=$7
+OUTPUT_DIR=$8
